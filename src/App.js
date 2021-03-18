b8122b0fc5f878db620fdd5f23f026e8fd1c6907
Joey Brown <gjoey.brown@gmail.com>
2021-03-18 13:51:16 -0700

    Added Nav and Footer components, installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import Results from "./components/Results";
 import Container from "./components/Container";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer"
 
 class App extends Component {
   render() {
     return(
       <Wrapper>
+        <Nav />
         <Title>Test Store</Title>
         <Container>
           <Results />
         </Container>
+        <Footer />
       </Wrapper>
     )
   }
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Title(props) {
-    return <h1>{props.children}</h1>;
+    return (
+    <h1>{props.children}</h1>
+    )
 }
 
 export default Title;
